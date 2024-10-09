505f3aaa0383cc86a2594d7877f6ee4b14d4c2a2
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-10-09 22:03:41 +0300

    created models for planetarium and user. Make changes in admin.py

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+    astronomy_show = models.ForeignKey(AstronomyShow, related_name="themes", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, related_name="sessions_ast", on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, related_name="sessions_pl", on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.astronomy_show.title} at {self.show_time}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name="reservations", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Reservation by {self.user.username} on {self.created_at}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, related_name="tickets", on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, related_name="tickets_re", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Row {self.row}, Seat {self.seat}"
